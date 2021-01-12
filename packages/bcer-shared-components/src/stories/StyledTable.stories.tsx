caa1bca2d94bc81baef796224b9375f259d6eb74
noel-fw <noel@freshworks.io>
2021-01-12 00:29:27 -0800

    styled table changes for bulk edit

--- packages/bcer-shared-components/src/stories/StyledTable.stories.tsx
+++ packages/bcer-shared-components/src/stories/StyledTable.stories.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { StyledTable } from '@/index';
+import { Typography } from '@material-ui/core';
+
+import { StyledButton } from '@/index';
 
 export default { title: 'Mui Styled table' }
 
@@ -25,15 +28,15 @@ export const WideTable = () => {
   return (
     <StyledTable
       columns={[
-        {title: 'First Name', field: 'firstName'},
-        {title: 'Last Name', field: 'lastName'},
-        {title: 'Address', field: 'address'},
-        {title: 'ex1', field: 'ex1'},
-        {title: 'ex1', field: 'ex1'},
-        {title: 'ex2', field: 'ex2'},
-        {title: 'ex3', field: 'ex3'},
-        {title: 'ex4', field: 'ex4'},
-        {title: 'ex5', field: 'ex5'},
+        {title: 'First Name', field: 'firstName', editable: 'never' },
+        {title: 'Last Name', field: 'lastName', editable: 'never' },
+        {title: 'Address', field: 'address', editable: 'never' },
+        {title: 'ex1', field: 'ex1', editable: 'never' },
+        {title: 'ex1', field: 'ex1', editable: 'never' },
+        {title: 'ex2', field: 'ex2', editable: 'never' },
+        {title: 'ex3', field: 'ex3', editable: 'never' },
+        {title: 'ex4', field: 'ex4' },
+        {title: 'ex5', field: 'ex5' },
       ]}
       data={[
         {firstName: 'My Name', lastName: 'A Surname', address: '1234 North S.E. West st.', ex1: 'somthing long', ex2: 'something longer', ex3: 'something else', ex4: 'something not else', ex6: 'the end'},
@@ -52,6 +55,27 @@ export const WideTable = () => {
         {firstName: 'Not my Name', lastName: 'A different Surname', address: 'something equally ridiculous', ex1: 'somthing long', ex2: 'something longer', ex3: 'something else', ex4: 'something not else', ex6: 'the end'},
         {firstName: 'Also not my Name', lastName: 'Another different Surname', address: 'something not so ridiculous', ex1: 'somthing long', ex2: 'something longer', ex3: 'something else', ex4: 'something not else', ex6: 'the end'},
       ]}
+      icons={{
+        Edit: forwardRef((props, ref) => (
+          <StyledButton variant='outlined' onClick={(event) => alert('Edit All')} >
+            Edit All
+          </StyledButton>
+        ))
+      }}
+      editable={{
+        onBulkUpdate: changes =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve(1);
+            }, 1000);
+          }),     
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve(1);
+            }, 1000);
+          }),     
+      }}
     />
   )
 }
