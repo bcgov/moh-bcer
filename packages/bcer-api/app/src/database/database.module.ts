7c2c19ed79301b799d72b0b5ca9e4aec6508eb02
Sagar Roy <sagar@freshworks.io>
2021-10-08 03:39:00 -0400

    added docker compose file for retailer application with a makefile

--- packages/bcer-api/app/src/database/database.module.ts
+++ packages/bcer-api/app/src/database/database.module.ts
@@ -2,10 +2,12 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseService } from './database.service';
+import { LOCAL_DB_CONFIG } from './local-db-config.entity';
 
+console.log(LOCAL_DB_CONFIG)
 @Module({
   imports: [
-    TypeOrmModule.forRoot()
+    TypeOrmModule.forRoot(process.env.NODE_ENV ==='development' ? LOCAL_DB_CONFIG : {})
   ],
   providers: [DatabaseService],
 })
