03c211b45558df464e2b42d7740628f1fec56250
Sagar Roy <sagar@freshworks.io>
2021-10-20 14:02:20 -0400

    added missing quatation mark

--- packages/bcer-api/app/src/migrations/1645910033222-AddLocationClosedAndNoiStatus.ts
+++ packages/bcer-api/app/src/migrations/1645910033222-AddLocationClosedAndNoiStatus.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddLocationClosedAndNoiStatus1645910033222
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE TYPE location_status_enum AS ENUM ('active', 'closed', 'deleted)`);
+    await queryRunner.query(`CREATE TYPE location_status_enum AS ENUM ('active', 'closed', 'deleted')`);
     await queryRunner.query(`ALTER TABLE "location" ADD COLUMN "status" location_status_enum NOT NULL DEFAULT 'active'`);
     await queryRunner.query(`ALTER TABLE "location" ADD COLUMN "closed_at" timestamp without time zone DEFAULT NULL`);
     await queryRunner.query(`ALTER TABLE "noi" ADD COLUMN "renewed_at" timestamp without time zone DEFAULT NULL`);
