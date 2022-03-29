66f7cbe227e838ca7126342088549b7ec10c6029
David Sabadash <DRsabadash@gmail.com>
2022-03-28 19:34:20 -0700

    updated migration as per PR comment

--- packages/bcer-api/app/src/migrations/1659555307942-FaqTable.ts
+++ packages/bcer-api/app/src/migrations/1659555307942-FaqTable.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class FaqTable1659555307942 implements MigrationInterface {
-    name = 'NotesTable1659555306536'
+    name = 'FaqTable1659555306536'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "faq" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "content" JSONB)`);
