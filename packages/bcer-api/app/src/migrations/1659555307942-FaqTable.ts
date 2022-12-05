5b59fbf56801e7155ded667f5e8adf53d910bbb9
chloe-yuu <c.yu@cgi.com>
2022-12-05 14:50:52 -0800

    update the gen uuid function

--- packages/bcer-api/app/src/migrations/1659555307942-FaqTable.ts
+++ packages/bcer-api/app/src/migrations/1659555307942-FaqTable.ts
@@ -4,7 +4,7 @@ export class FaqTable1659555307942 implements MigrationInterface {
     name = 'FaqTable1659555306536'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "faq" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "content" JSONB)`);
+        await queryRunner.query(`CREATE TABLE "faq" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "content" JSONB)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
