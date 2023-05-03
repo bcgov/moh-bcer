bd8fb045adeb55ec36a6fffee01617728aff310d
chloe-yuu <c.yu@cgi.com>
2023-05-03 12:50:35 -0700

    retailer user guide

--- packages/bcer-retailer-app/app/src/components/SideNav.tsx
+++ packages/bcer-retailer-app/app/src/components/SideNav.tsx
@@ -93,20 +93,7 @@ const useStyles = makeStyles({
   }
 });
 
-function ListItemLink(props: ListItemProps<'a', { button?: true }>) {
-  const history = useHistory();
-  const classes = useStyles();
-
-  return (
-    <ListItem button component="a" className={classes.listItem}
-      onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        event.preventDefault();
-        history.push(props.href);
-      }} {...props} />
-  )
-}
-
-interface IconProps { formStep: 'myDashboard' | 'myBusiness' | 'noi' | 'productReport' | 'manufacturingReport' | 'salesReports' };
+interface IconProps { formStep: 'myDashboard' | 'myBusiness' | 'noi' | 'productReport' | 'manufacturingReport' | 'salesReports' | 'userGuide'};
 
 export default function SideNav() {
   const { pathname } = useLocation();
@@ -145,6 +132,41 @@ export default function SideNav() {
       setAppGlobal({...appGlobal, networkErrorMessage: formatError(error)})
     }
   }, [error])
+
+  
+  function ListItemLink(props: ListItemProps<'a', { button?: true }>) {
+    const history = useHistory();
+    const classes = useStyles();
+
+    if (props.download) { //BCER User Guide 
+      return (
+        <ListItem button component="a" className={classes.listItem}
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+          setAppGlobal(() => {
+            return {
+              ...appGlobal,
+              userGuideComplete: true
+            }
+          });
+          event.preventDefault();
+          const link = document.createElement('a');
+          link.href = '../../public/BCER User Guide (2023).pdf';
+          link.download = 'BCER User Guide (2023).pdf';
+          link.target = '_blank';
+          link.click();
+        }} {...props} />
+      );
+    }
+
+    return (
+      <ListItem button component="a" className={classes.listItem}
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+          event.preventDefault();
+          history.push(props.href);
+        }} {...props} />
+    )
+  }
+
 
   const NavItem: React.SFC<IconProps> = ({ formStep }) => {
     let completedStep = false;
@@ -193,9 +215,16 @@ export default function SideNav() {
         stepTitle = 'Sales Reports';
         href = '/sales';
         showWarningIcon = false;
+        break;
+      case 'userGuide':
+        completedStep = appGlobal.userGuideComplete;
+        pathName = '/BCERUserGuide';
+        stepTitle = 'BCER User Guide';
+        showWarningIcon = false;
+        break;
     }
     return (
-      <ListItemLink href={appGlobal?.myBusinessComplete ? href : '#'} disabled={!appGlobal?.myBusinessComplete}>
+      <ListItemLink href={appGlobal?.myBusinessComplete ? href : '#'} disabled={!appGlobal?.myBusinessComplete} download={formStep==='userGuide'}>
         <ListItemIcon>
           {completedStep ?
             <CheckCircleOutlineOutlined
@@ -241,6 +270,7 @@ export default function SideNav() {
         <NavItem formStep={'productReport'} />
         <NavItem formStep={'manufacturingReport'} />
         <NavItem formStep={'salesReports'} />
+        <NavItem formStep={'userGuide'} />
         {
           keycloak.authenticated && (
             <div className={classes.logoutContainer} onClick={logout}>
