a010918a9aed15e73ba037a8894a8fc40b069ea0
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2024-03-25 14:55:20 -0700

    Update aurora-v2.tf

--- Infrastructure/aurora-v2.tf
+++ Infrastructure/aurora-v2.tf
@@ -36,7 +36,7 @@ resource "aws_db_subnet_group" "bcer_subnet_group" {
 
 data "aws_rds_engine_version" "postgresql" {
   engine  = "aurora-postgresql"
-  version = "13"
+  version = "13.12"
 }
 
 module "aurora_postgresql_v2" {
@@ -68,12 +68,6 @@ module "aurora_postgresql_v2" {
 
   db_parameter_group_name         = aws_db_parameter_group.bcer_postgresql13.id
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.bcer_postgresql13.id
-
-  lifecycle {
-    ignore_changes = [
-      engine_version,
-    ]
-  }
 
   serverlessv2_scaling_configuration = {
     min_capacity = var.aurora_acu_min
