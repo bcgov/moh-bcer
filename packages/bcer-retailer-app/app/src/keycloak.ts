8a3385fe56962d6808a013df36a30c92058d2386
chloe-yuu <c.yu@cgi.com>
2024-05-24 17:15:33 -0700

    add back

--- packages/bcer-retailer-app/app/src/keycloak.ts
+++ packages/bcer-retailer-app/app/src/keycloak.ts
@@ -4,7 +4,8 @@ const keycloak: Keycloak.KeycloakInstance = new (Keycloak as any)({
   realm: process.env.KEYCLOAK_REALM,
   url: process.env.BASE_KEYCLOAK_URL,
   clientId: process.env.KEYCLOAK_CLIENTID,
-  silentCheckSsoFallback: false
+  silentCheckSsoFallback: false,
+  onLoad: "login-required",
 });
 
 export default keycloak;
